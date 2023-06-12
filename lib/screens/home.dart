8e86f3282f2961804e13c6a94a02da1935a3f650
Mohammad Hasib <hasibhabibzadah786@gmail.com>
2023-06-12 10:11:10 +0430

    update appbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myroutine/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super (key: key);
@@ -8,7 +9,28 @@ class Home extends StatelessWidget {
 
   Widget build(BuildContext context){
     return  Scaffold(
-      appBar: AppBar(title: Text('Todo App')),
+      appBar: AppBar(
+        backgroundColor: tdBgColor,
+        title : Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30
+          ),
+          Container(
+            height:40 ,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.png'),
+            ),
+          )
+        ],
+        )
+      ),
+
       body: Container(
         child: Text("This is my home screen"),
       ),
