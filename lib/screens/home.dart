4deb45562d87886629195d120db4bedabfa6c617
Mohammad Hasib <hasibhabibzadah786@gmail.com>
2023-06-20 09:21:20 +0430

    debugging some problems

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,7 +4,11 @@ import '../model/ToDo.dart';
 import '../widgets/todo_item.dart';
 class Home extends StatefulWidget {
 
-  Home({Key? key}) : super (key: key);
+  Home({Key? key, required this.todo, this.onToDoChange, this.ondeleteItem}) : super (key: key);
+  final ToDo todo;
+  final onToDoChange;
+  final ondeleteItem;
+
 
   @override
   State<Home> createState() => _HomeState();
@@ -18,84 +22,87 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context){
     return  Scaffold(
       appBar: buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
-        child: Column(
-          children: [
-           searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Stack(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+            child: Column(
+              children: [
+               searchBox(),
+                Expanded(
+                  child: ListView(
                     children: [
-                      Container(
-                        margin: EdgeInsets.only(top: 15, bottom: 20),
-                        child: Text(
-                          'All Routines ',
-                          style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.w500
-                          ),
-                        ),
-                      ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Row(children: [
-                        Expanded(child: Container(
-                          margin: EdgeInsets.only(
-                            bottom: 20,
-                            right: 20,
-                            left: 20,
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 5),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            boxShadow: const [BoxShadow(
-                              color: Colors.grey,
-                              offset: Offset(0.0, 0.0),
-                              blurRadius: 10.0,
-                              spreadRadius: 0.0,
-
-                            ),],
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              hintText: 'Add a new todo item',
-                              border: InputBorder.none
-                            ),
-                          ),
-                        ),
-                        ),
+                      Stack(
+                        children: [
                           Container(
-                                margin: EdgeInsets.only(
-                                bottom: 20, right: 20
-                            ),
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                primary: tdBlue,
-                                minimumSize: Size(60,60),
-                                elevation: 10,
-
+                            margin: EdgeInsets.only(top: 15, bottom: 20),
+                            child: Text(
+                              'All Routines ',
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500
                               ),
-                              onPressed: () {  },
-                              child: Text('+', style: TextStyle(fontSize: 40),),
                             ),
                           ),
-                      ],),
-                    )
+
+                        ],
+                      ),
+                      for(ToDo todoo in todoList)
+                      TodoItem(todo: todoo,
+                        onTodoChnaged: __handleToBeChanged,
+                        onDeleteItem: (){},
+                      ),
                     ],
                   ),
-                  for(ToDo todoo in todoList)
-                  TodoItem(todo: todoo,
-                    onTodoChnaged: __handleToBeChanged,
-                    onDeleteItem: (){},
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(child: Container(
+                  margin: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration:  BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Colors.grey,
+                        offset: Offset(0.0,0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                ],
-              ),
-            )
-          ],
-        ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add New Todo Item',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: tdBlue,
+                      minimumSize: Size(60, 60),
+                      elevation: 10
+                    ),
+                    onPressed: () {  },
+                    child: Text("+",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -105,6 +112,8 @@ class _HomeState extends State<Home> {
     todo.isDone = !todo.isDone;
     });
   }
+
+
 
 
   Widget searchBox(){
